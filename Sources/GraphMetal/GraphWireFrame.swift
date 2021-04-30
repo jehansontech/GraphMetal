b2ccf5df8aebcab723a297ffe966bc029393a785
Jim Hanson <jim@jehanson.org>
2021-04-30 07:11:43 -0400

    debugs

--- Sources/GraphMetal/GraphWireFrame.swift
+++ Sources/GraphMetal/GraphWireFrame.swift
@@ -98,7 +98,7 @@ class GraphWireFrame<N: RenderableNodeValue, E: RenderableEdgeValue>: Renderable
         E == H.GraphType.EdgeType.ValueType,
         N == H.GraphType.NodeType.ValueType {
 
-        print("GraphWireFrame.prepareUpdate: graphHolder = \(graphHolder)")
+        // print("GraphWireFrame.prepareUpdate")
 
         if  graphHolder.hasTopologyChanged(since: lastTopologyUpdate) {
             self.prepareTopologyUpdate(graphHolder.graph)
