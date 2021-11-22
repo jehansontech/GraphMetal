bc156e1e5d179451ae0f1ca424b26d56efe67bcd
Jim Hanson <jim@jehanson.org>
2021-11-22 17:43:14 -0500

    ttt

--- Sources/GraphMetal/GraphWireFrame.swift
+++ Sources/GraphMetal/GraphWireFrame.swift
@@ -247,8 +247,8 @@ class GraphWireFrame<N: RenderableNodeValue, E: RenderableEdgeValue> {
                                              Float(edgeColor.y),
                                              Float(edgeColor.z),
                                              Float(edgeColor.w))
-        uniforms[0].zFadeOffset = 0
-        uniforms[0].zFadeFactor = 1
+        uniforms[0].zFadeOffset = 1
+        uniforms[0].zFadeFactor = 0
     }
     
     func encodeCommands(_ renderEncoder: MTLRenderCommandEncoder) {
