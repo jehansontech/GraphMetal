b5bf0577cfd39f8add41129f170351d68942187b
Jim Hanson <jim@jehanson.org>
2021-12-12 10:59:25 -0500

    catch up macos snapshot

--- Sources/GraphMetal/GraphRenderer.swift
+++ Sources/GraphMetal/GraphRenderer.swift
@@ -23,10 +23,14 @@ enum RendererError: Error {
     case bufferCreationFailed
 }
 
+/// This protocol exists so that we can extend it, e.g., in Snapshots.swift
+@objc protocol GraphRendererProtocol: MTKViewDelegate {
+}
+
 ///
 ///
 ///
-public class GraphRendererBase<S: RenderableGraphHolder>: NSObject, MTKViewDelegate, RenderControllerDelegate {
+public class GraphRendererBase<S: RenderableGraphHolder>: NSObject, GraphRendererProtocol, RenderControllerDelegate {
 
     public typealias NodeValueType = S.GraphType.NodeType.ValueType
 
@@ -160,10 +164,10 @@ public class GraphRendererBase<S: RenderableGraphHolder>: NSObject, MTKViewDeleg
     public func findNearestNode(_ clipLocation: SIMD2<Float>) -> NodeID? {
 
         if let (nn, _) = graphHolder.graph.findNearestNode(clipLocation,
-                                                 projectionMatrix: self.projectionMatrix,
-                                                 modelViewMatrix: self.modelViewMatrix,
-                                                 zNear: self.renderController.zNear,
-                                                 zFar: self.renderController.zFar) {
+                                                           projectionMatrix: self.projectionMatrix,
+                                                           modelViewMatrix: self.modelViewMatrix,
+                                                           zNear: self.renderController.zNear,
+                                                           zFar: self.renderController.zFar) {
             return nn.id
         }
         else {
@@ -222,7 +226,7 @@ public class GraphRendererBase<S: RenderableGraphHolder>: NSObject, MTKViewDeleg
         }
 
         if screenshotRequested {
-            takeScreenshot(view)
+            saveSnapshot(view)
             screenshotRequested = false
         }
 
@@ -245,8 +249,8 @@ public class GraphRendererBase<S: RenderableGraphHolder>: NSObject, MTKViewDeleg
                 renderEncoder.setDepthStencilState(depthState)
 
                 wireFrame.encodeCommands(renderEncoder) //,
-                                    // dynamicUniformBuffer,
-                                    // uniformBufferOffset)
+                                                        // dynamicUniformBuffer,
+                                                        // uniformBufferOffset)
 
                 renderEncoder.endEncoding()
                 commandBuffer.present(drawable)
@@ -260,8 +264,8 @@ public class GraphRendererBase<S: RenderableGraphHolder>: NSObject, MTKViewDeleg
             renderController.updateCompleted()
         }
 
-//        let dt = Date().timeIntervalSince(t0)
-//        debug("GraphRenderer.draw", "done. dt=\(dt)")
+        //        let dt = Date().timeIntervalSince(t0)
+        //        debug("GraphRenderer.draw", "done. dt=\(dt)")
     }
     
     
@@ -274,73 +278,29 @@ public class GraphRendererBase<S: RenderableGraphHolder>: NSObject, MTKViewDeleg
         self.modelViewMatrix = Self.makeModelViewMatrix(pov)
 
         wireFrame.preDraw(projectionMatrix: projectionMatrix,
-                               modelViewMatrix: modelViewMatrix,
-                               pov: pov,
-                               fadeoutOnset: renderController.fadeoutOnset,
-                               fadeoutDistance: renderController.fadeoutDistance)
+                          modelViewMatrix: modelViewMatrix,
+                          pov: pov,
+                          fadeoutOnset: renderController.fadeoutOnset,
+                          fadeoutDistance: renderController.fadeoutDistance)
 
 
-//        let dt = Date().timeIntervalSince(t0)
-//        if (dt > 1/30) {
-//            debug("Renderer", "predraw: elapsed time \(dt)")
-//        }
+        //        let dt = Date().timeIntervalSince(t0)
+        //        if (dt > 1/30) {
+        //            debug("Renderer", "predraw: elapsed time \(dt)")
+        //        }
 
     }
 
-    private func takeScreenshot(_ view: MTKView) {
-
-        // Adapted from
-        // https://stackoverflow.com/questions/33844130/take-a-snapshot-of-current-screen-with-metal-in-swift
-        // [accessed 04/2021]
-
-        guard
-            let texture = view.currentDrawable?.texture
-        else {
-            return
-        }
-
-        let width = texture.width
-        let height   = texture.height
-        let rowBytes = texture.width * 4
-        let p = malloc(width * height * 4)
-        texture.getBytes(p!, bytesPerRow: rowBytes, from: MTLRegionMake2D(0, 0, width, height), mipmapLevel: 0)
-
-        let pColorSpace = CGColorSpaceCreateDeviceRGB()
-
-        let rawBitmapInfo = CGImageAlphaInfo.noneSkipFirst.rawValue | CGBitmapInfo.byteOrder32Little.rawValue
-        let bitmapInfo:CGBitmapInfo = CGBitmapInfo(rawValue: rawBitmapInfo)
-
-        let selftureSize = texture.width * texture.height * 4
-        let releaseMaskImagePixelData: CGDataProviderReleaseDataCallback = { (info: UnsafeMutableRawPointer?, data: UnsafeRawPointer, size: Int) -> () in
-            return
-        }
-        let provider = CGDataProvider(dataInfo: nil, data: p!, size: selftureSize, releaseData: releaseMaskImagePixelData)
-        let cgImage = CGImage(width: texture.width,
-                              height: texture.height,
-                              bitsPerComponent: 8,
-                              bitsPerPixel: 32,
-                              bytesPerRow: rowBytes,
-                              space: pColorSpace,
-                              bitmapInfo: bitmapInfo,
-                              provider: provider!,
-                              decode: nil,
-                              shouldInterpolate: true,
-                              intent: CGColorRenderingIntent.defaultIntent)
-
-        if let cgImage = cgImage {
-
-#if os(iOS)
-            let uiImage = UIImage(cgImage: cgImage)
-            UIImageWriteToSavedPhotosAlbum(uiImage, nil, nil, nil)
-#elseif os(macOS)
-            // TODO
-#endif
+    func saveSnapshot(_ view: MTKView) {
+        if let cgImage = view.takeSnapshot() {
+            self.saveImage(cgImage)
         }
     }
 
-}
+} // end class GraphRendererBase
 
-#if os(iOS)
+#if os(iOS) // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 public class GraphRenderer<S: RenderableGraphHolder>: GraphRendererBase<S>, UIGestureRecognizerDelegate {
 
     @objc func tap(_ gesture: UITapGestureRecognizer) {
@@ -481,7 +441,6 @@ public class GraphRenderer<S: RenderableGraphHolder>: GraphRendererBase<S>, UIGe
 
     /// needed in order to do simultaneous gestures
     public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith: UIGestureRecognizer) -> Bool {
-        // print("simultaneous gestures: \(describeGR(gestureRecognizer)) + \(describeGR(shouldRecognizeSimultaneouslyWith))")
         if gestureRecognizer is UIPanGestureRecognizer || shouldRecognizeSimultaneouslyWith is UIPanGestureRecognizer {
             return false
         }
@@ -489,7 +448,9 @@ public class GraphRenderer<S: RenderableGraphHolder>: GraphRendererBase<S>, UIGe
         return true
     }
 }
-#elseif os(macOS)
+
+#elseif os(macOS)  // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 public class GraphRenderer<S: RenderableGraphHolder>: GraphRendererBase<S>, NSGestureRecognizerDelegate {
 
     @objc func tap(_ gesture: NSClickGestureRecognizer) {
@@ -625,36 +586,20 @@ public class GraphRenderer<S: RenderableGraphHolder>: GraphRendererBase<S>, NSGe
 
     /// needed in order to do simultaneous gestures
     public func gestureRecognizer(_ gestureRecognizer: NSGestureRecognizer, shouldRecognizeSimultaneouslyWith: NSGestureRecognizer) -> Bool {
-        // print("simultaneous gestures: \(describeGR(gestureRecognizer)) + \(describeGR(shouldRecognizeSimultaneouslyWith))")
         if gestureRecognizer is NSPanGestureRecognizer || shouldRecognizeSimultaneouslyWith is NSPanGestureRecognizer {
             return false
         }
 
         return true
     }
-
 }
-#endif
+
+#endif // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 
 // ======================================================================
 // MARK:- conversion to clip space
 // ======================================================================
-
-fileprivate func clipX(_ viewX: CGFloat, _ viewWidth: CGFloat) -> Float {
-    return Float(2 * viewX / viewWidth - 1)
-}
-
-#if os(iOS)
-fileprivate func clipY(_ viewY: CGFloat, _ viewHeight: CGFloat) -> Float {
-    // In iOS, viewY increases toward the TOP of the screen
-    return Float(1 - 2 * viewY / viewHeight)
-}
-#elseif os(macOS)
-fileprivate func clipY(_ viewY: CGFloat, _ viewHeight: CGFloat) -> Float {
-    // In macOS, viewY increaases toward the BOTTOM of the screen
-    return Float(2 * viewY / viewHeight - 1)
-}
-#endif
 
 fileprivate func clipPoint(_ viewPt: CGPoint, _ viewSize: CGRect) -> SIMD2<Float> {
     return SIMD2<Float>(clipX(viewPt.x, viewSize.width), clipY(viewPt.y, viewSize.height))
@@ -665,47 +610,22 @@ fileprivate func clipPoint(_ viewPt0: CGPoint, _ viewPt1: CGPoint, _ viewSize: C
                         clipY((viewPt0.y + viewPt1.y)/2, viewSize.height))
 }
 
-#if os(iOS)
-fileprivate func describeGR(_ gr: UIGestureRecognizer) -> String {
-
-    let grName: String
-    if gr is UILongPressGestureRecognizer {
-        grName = "longPress"
-    }
-    else if gr is UIPanGestureRecognizer {
-        grName = "pan"
-    }
-    else if gr is UIPinchGestureRecognizer {
-        grName = "pinch"
-    }
-    else if gr is UIRotationGestureRecognizer {
-        grName = "rotation"
-    }
-    else if (gr is UITapGestureRecognizer) {
-        grName = "tap"
-    }
-    else {
-        grName = "\(gr)"
-    }
-
-    let stateString: String
-    switch (gr.state) {
-    case .began:
-        stateString = "began"
-    case .cancelled:
-        stateString = "cancelled"
-    case .changed:
-        stateString = "changed"
-    case .ended:
-        stateString = "ended"
-    case .failed:
-        stateString = "failed"
-    case .possible:
-        stateString = "possible"
-    @unknown default:
-        stateString = "(unknown)"
-    }
-
-    return "\(grName) \(stateString)"
+fileprivate func clipX(_ viewX: CGFloat, _ viewWidth: CGFloat) -> Float {
+    return Float(2 * viewX / viewWidth - 1)
 }
-#endif
+
+#if os(iOS)  // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+fileprivate func clipY(_ viewY: CGFloat, _ viewHeight: CGFloat) -> Float {
+    // In iOS, viewY increases toward the TOP of the screen
+    return Float(1 - 2 * viewY / viewHeight)
+}
+
+#elseif os(macOS)  // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+fileprivate func clipY(_ viewY: CGFloat, _ viewHeight: CGFloat) -> Float {
+    // In macOS, viewY increaases toward the BOTTOM of the screen
+    return Float(2 * viewY / viewHeight - 1)
+}
+
+#endif  // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
---
+++ Sources/GraphMetal/Snapshot.swift
@@ -0,0 +1,94 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jim Hanson on 12/12/21.
+//
+
+import MetalKit
+
+extension MTKView {
+
+    func takeSnapshot() -> CGImage? {
+        guard
+            let texture = self.currentDrawable?.texture
+        else {
+            return nil
+        }
+
+        let width = texture.width
+        let height   = texture.height
+        let rowBytes = texture.width * 4
+        let p = malloc(width * height * 4)
+        texture.getBytes(p!, bytesPerRow: rowBytes, from: MTLRegionMake2D(0, 0, width, height), mipmapLevel: 0)
+
+        let pColorSpace = CGColorSpaceCreateDeviceRGB()
+
+        let rawBitmapInfo = CGImageAlphaInfo.noneSkipFirst.rawValue | CGBitmapInfo.byteOrder32Little.rawValue
+        let bitmapInfo:CGBitmapInfo = CGBitmapInfo(rawValue: rawBitmapInfo)
+
+        let selftureSize = texture.width * texture.height * 4
+        let releaseMaskImagePixelData: CGDataProviderReleaseDataCallback = { (info: UnsafeMutableRawPointer?, data: UnsafeRawPointer, size: Int) -> () in
+            return
+        }
+        let provider = CGDataProvider(dataInfo: nil, data: p!, size: selftureSize, releaseData: releaseMaskImagePixelData)
+        return CGImage(width: texture.width,
+                       height: texture.height,
+                       bitsPerComponent: 8,
+                       bitsPerPixel: 32,
+                       bytesPerRow: rowBytes,
+                       space: pColorSpace,
+                       bitmapInfo: bitmapInfo,
+                       provider: provider!,
+                       decode: nil,
+                       shouldInterpolate: true,
+                       intent: CGColorRenderingIntent.defaultIntent)
+    }
+}
+
+#if os(iOS) // =========================================================================
+
+
+extension GraphRendererProtocol {
+
+    func saveImage(_ cgImage: CGImage) {
+        let uiImage = UIImage(cgImage: cgImage)
+        UIImageWriteToSavedPhotosAlbum(uiImage, nil, nil, nil)
+    }
+}
+
+#elseif os(macOS) // =========================================================================
+
+
+extension GraphRendererProtocol {
+
+    func saveImage(_ cgImage: CGImage) {
+        let nsImage = NSImage(cgImage: cgImage, size: NSSize(width: cgImage.width, height: cgImage.height))
+        let desktopURL = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask).first!
+        let destinationURL = desktopURL.appendingPathComponent("my-image.png")
+        if nsImage.pngWrite(to: destinationURL, options: .withoutOverwriting) {
+                       print("File saved")
+        }
+    }
+}
+
+extension NSImage {
+
+    var pngData: Data? {
+        guard let tiffRepresentation = tiffRepresentation, let bitmapImage = NSBitmapImageRep(data: tiffRepresentation) else { return nil }
+        return bitmapImage.representation(using: .png, properties: [:])
+    }
+
+    func pngWrite(to url: URL, options: Data.WritingOptions = .atomic) -> Bool {
+        do {
+            try pngData?.write(to: url, options: options)
+            return true
+        } catch {
+            print(error)
+            return false
+        }
+    }
+}
+
+#endif // =========================================================================
+
